9a8d9f08c94f436ac97f110f879125e6a88f3ce9
Civodel <tucanbarrera@live.com.mx>
2024-03-13 00:15:31 -0600

    first api with FastApi

---
+++ Python/PythonBackend/FastApiApi/routes/users.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# uvicorn users:users --reload
+
+class User(BaseModel):
+    id:int
+    name:str
+    surname:str
+    age:int
+    game_id:int
+
+
+
+users_list=[User(id=1,name="Tilapia",surname="Madona",age=12,game_id=1500),User(id=2,name="Paul",surname="estoker",age=12,game_id=1500)]
+@app.get("/users")
+async def get_users_list()->list:
+
+    return users_list
+
+@app.get(f"/user/{id}")
+async def get_user(id:int)->dict:
+    filter_user=filter(lambda user: user.id == id,users_list)
+    return list(filter_user)
+
