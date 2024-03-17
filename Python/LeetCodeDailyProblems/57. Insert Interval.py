92f3f4703a0e83d1b348eea6d63a7cba4847ced7
Civodel <tucanbarrera@live.com.mx>
2024-03-17 14:05:04 -0600

    letcode 57 problem

--- Python/LeetCodeDailyProblems/57. Insert Interval.py
+++ Python/LeetCodeDailyProblems/57. Insert Interval.py
@@ -14,38 +14,44 @@ class Solution:
 
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         inter=[]
-
         if not intervals:
             return [newInterval]
-
         for interval in intervals:
-            inter_count=0
-            dummy_inter=newInterval
 
-            if newInterval[0]>interval[1] and newInterval[1]>interval[1]:
-                inter.append(interval)
+            if newInterval[0] > interval[1] and newInterval[1]>interval[1]:
+                if len(intervals) ==1 :
+                    inter.append(interval)
+                    inter.append(newInterval)
+                else:
+                    inter.append(interval)
             elif newInterval[0]<interval[0] and newInterval[1]<interval[0]:
-                inter.append(interval)
 
+                inter.append(newInterval)
+                inter.append(interval)
             elif interval[0]<newInterval[0] and newInterval[1]<interval[1]:
                 inter.append(interval)
 
             else:
 
-                if interval[0] < newInterval[0]:
+             interval_max=max(interval)
+             if newInterval[0]>interval[0]:
                     newInterval[0] = interval[0]
-                    inter.append(newInterval)
-                elif newInterval[1]<interval[1]:
+             elif newInterval[1]<interval[1]:
                     newInterval[1] = interval[1]
+             if newInterval[1] == interval_max:
 
-                elif newInterval[0] <= interval[1] and newInterval[1] <=interval[1]:
-                    inter.append(newInterval)
+                 inter.append(newInterval)
+
+
 
         return  inter
 
 if __name__ == '__main__':
-    list1 = [[1,3],[6,9]]
-    newInterval =[2,5]
+    list1 =[[1,5],[6,8]]
+
+    newInterval =[5,6]
+
+
 
 
     print(Solution().insert(list1, newInterval))
